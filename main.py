38dbe881b7034a91e3ac7846a35ae1ebfe0a072a
jsantore <jsantore@bridgew.edu>
2022-02-02 15:51:00 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+
+def main():
+
+    print("just doing other  work here")
+    user_response = int(input("what is your age?"))
+    half_age = user_response/2
+    print(f"{half_age} is half your age")
+
+main()
