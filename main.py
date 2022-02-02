69ac555b7bcef4ad6b1dd69d840b3f2572aaeb40
jsantore <jsantore@bridgew.edu>
2022-02-02 15:54:49 -0500

    add line for demo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     print("just doing other  work here")
     user_response = int(input("what is your age?"))
     half_age = user_response/2
-    age_squared = half_age*half_age
+    # change
     print(f"{half_age} is half your age")
 
 main()
